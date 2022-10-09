a22868b9ec2761e3e734b324d64ab78ce751e036
egor <grebnevegor2000@mail.ru>
2022-10-09 21:24:56 +0300

    user contrller update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const bcrypt = require('bcrypt')
+
 
 class UserController {
-    async test(req, res) {
+    async createUser(req, res) {
         try {
-            res.status(200).json({ data: "ok" });
+            // const validator = validationResult(req);
+
+            const { username, email, password } = req.body;
+
+
+            const user = await User.create(username, email, await bcrypt.hash(password, 11));
+
+            res.status(201).json([{
+                user: user
+            }])
+
         } catch (error) {
             res.status(500).json([{ msg: "Something went wrong, try one more time" }, { dev_message: error.message }])
         }
