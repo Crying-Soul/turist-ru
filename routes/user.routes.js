f84945a8bf5f9b6663539efd8b12714ca10b62a3
egor <grebnevegor2000@mail.ru>
2022-10-12 18:37:42 +0300

    validation & routing

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,23 @@
 const { Router } = require("express");
+const { body, check } = require('express-validator');
 
 const userController = require("../controllers/user.controller");
 
 const router = Router();
 
 
-router.post("/user/create", userController.createUser);
+router.post("/user/create",
+    check('username')
+    .exists()
+    .withMessage('username is required')
+    .isLength({ min: 3 })
+    .withMessage('wrong username length'),
+    check('email').isEmail().withMessage('invalid email').normalizeEmail(),
+    check('password')
+    .exists()
+    .withMessage('password is required').isLength({
+        min: 3
+    }), userController.createUser);
 
 
 module.exports = router;
