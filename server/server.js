b9b3db0c4c703b5377ff139b49cd434f72a719a9
egor <grebnevegor2000@mail.ru>
2022-10-12 18:36:33 +0300

    custom 404 error page

--- server/server.js
+++ server/server.js
@@ -10,20 +10,31 @@ const mapRouter = require("../routes/map.routes");
 class Server {
     constructor() {
         this.PORT = process.env.PORT || 5000;
-        this.ROOT = require("path").dirname(require.main.filename);
+        this.ROOT = path.dirname(require.main.filename);
         this.app = express();
+        this.localIp = Object.values(os.networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat((i.family === "IPv4" && !i.internal && i.address) || []), [])), [])[0];
     }
-    getLocalIp() {
-        return Object.values(os.networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat((i.family === "IPv4" && !i.internal && i.address) || []), [])), [])[0];
-    }
+
     start() {
         this.app.listen(this.PORT, () => {
-            console.log(`App listening at ${this.getLocalIp()}:${this.PORT}`);
+            console.log(`App listening at ${this.localIp}:${this.PORT}`);
         });
         this.app.use(bodyParser.json());
         this.app.use(cors());
+
         this.app.use("/api", userRouter);
         this.app.use("/api", mapRouter);
+
+        this.app.all("*", (req, res, next) => {
+            // console.log(`Got ${req.method} request from ${req.headers['x-forwarded-for'] || req.socket.remoteAddress}`);
+            res.status(404);
+            res.render('404.jade', { title: '404: File Not Found' });
+
+        });
+
+
+
+
     }
 }
 
