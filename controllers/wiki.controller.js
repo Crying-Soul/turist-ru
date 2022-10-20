d58212608acd00689d073df43e5a66870fe504c2
Crying-Soul <grebnevegor2000@mail.ru>
2022-10-21 02:09:27 +0300

    .

--- controllers/wiki.controller.js
+++ controllers/wiki.controller.js
@@ -10,7 +10,8 @@ class WikiController{
 
             if (validator.isEmpty()) {
                
-                res.status(200).json(await Wiki.getInfo(query));
+               // res.status(200).json(await Wiki.getInfo(query));
+		    res.sendStatus(204)
                 return;
             }
 
