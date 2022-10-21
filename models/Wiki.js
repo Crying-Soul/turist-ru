eb483b6a1ba6ebd378f7eae8cfbc6995dcdec462
Crying-Soul <grebnevegor2000@mail.ru>
2022-10-21 05:20:43 +0300

    .

--- models/Wiki.js
+++ models/Wiki.js
@@ -4,7 +4,7 @@ const Request = require('./Request')
 class Wiki{
     async getInfo(query){
 	
-        return (await Request.get(`https://ru.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json`))
+        return (await Request.get(`https://ru.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json`)).search[0].snippet
     }
 }
 
