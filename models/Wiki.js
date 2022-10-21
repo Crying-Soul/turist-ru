ced6a844953ef49cf360a035af55da14a3f9b666
Пользователь <User@ITL-COW-2.stud.kpfu.ru>
2022-10-21 13:48:05 +0300

    normalize

--- models/Wiki.js
+++ models/Wiki.js
@@ -6,7 +6,8 @@ class Wiki{
 	
         const mainData = (await Request.get(`https://ru.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json`)).query.search[0]                   
         mainData.thumb = (await Request.get(`http://ru.wikipedia.org/w/api.php?action=query&titles=${mainData.title}&prop=pageimages&format=json&pithumbsize=100`)).query.pages[mainData.pageid].thumbnail;                   
-        return mainData;
+        const {title, snippet, thumb: source} = mainData
+        return  Object.assign({}, {title, text:snippet, ...source});
     }
 }
 
