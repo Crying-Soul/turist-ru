abbb3321e650049a3f1430308beec81b9f98a60a
Пользователь <User@ITL-COW-2.stud.kpfu.ru>
2022-10-21 10:16:25 +0300

    wiki

--- models/Wiki.js
+++ models/Wiki.js
@@ -4,7 +4,9 @@ const Request = require('./Request')
 class Wiki{
     async getInfo(query){
 	
-        return (await Request.get(`https://ru.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json`)).query.search[0].snippet                     
+        const mainData = (await Request.get(`https://ru.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&utf8=&format=json`)).query.search[0]                   
+        mainData.thumb = (await Request.get(`http://ru.wikipedia.org/w/api.php?action=query&titles=${mainData.title}&prop=pageimages&format=json&pithumbsize=100`)).query.pages[mainData.pageid].thumbnail;                   
+        return mainData;
     }
 }
 
